4bf0ce2159a8f607948a3601f340972d1bc111d2
1228Uttkarsh <79569546+1228Uttkarsh@users.noreply.github.com>
2023-07-10 21:06:21 +0530

    Day4.js

--- Day4.js
+++ Day4.js
@@ -1,17 +1,27 @@
-// let http = require('http')
+//How to use modules in nodejs.
+//require(module_name) function is use to modules
 
+ var http = require('http')
 // http.createServer(function(req,res){
-//     res.writeHead(200,{'Content-Type':'text/html'});
-//     res.end('Hello World!');
+//   res.writeHead(200,{'content-Type': 'text/html'});
+//   res.end('Hello world');
+  
 // }).listen(8080);
 
-
-// //Creating my own module to print the current date and Time
-let http = require('http')
-let dt = require('./myFirstModule')
-
+//Include module -> if user want to use his own module then they can save the js file and use export keyword to use the user defined module outside the module file 
+// Below is the example
+var dt = require('./myfirstModule')//accessing the module which is created  to use the external module.
+//The example is to show the current date and time on the server.
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("The date and time are currently:"+ dt.myDateTime());
-    res.end()
-})
+  res.writeHead(200,{'content-Type':'text/html'});
+  res.write("The date and time are currently: "+dt.myDateTime());
+  res.end("Completed!")
+}).listen(8080)
+
+
+/*
+    //Here is the code that is saved in myfirstModule is below
+    exports.myDateTime = function(){
+    return Date();
+};
+    */
